639d3b82c8f65fd484f5eef29b2e307563663158
SaimTasleem111 <saimtasleem5@gmail.com>
2026-05-11 16:27:50 +0500

    Fix DATABASE_URL config

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-engine=create_engine(settings.Database_URL)
+engine=create_engine(settings.DATABASE_URL)
 LocalSession = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 Base= declarative_base()
